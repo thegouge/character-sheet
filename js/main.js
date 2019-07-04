9ba5dc3bf31327df6eaaa7995fb1a9a26ac689ba
thegouge <thegouge42@gmail.com>
2019-07-03 17:40:00 -0700

    added page initilization function

--- js/main.js
+++ js/main.js
@@ -9,6 +9,95 @@ import {backgrounds} from "./data/backgrounds.js";
 
 const character = new Character();
 
+function initializePage() {
+  const classList = document.getElementById("class-list");
+  const backgroundList = document.getElementById("background-list");
+  const raceList = document.getElementById("race-list");
+
+  // Populate Class List
+  classes.forEach((playerClass) => {
+    classList.innerHTML += createListItem(playerClass.name);
+  });
+
+  // Populate Backround List
+  backgrounds.forEach((background) => {
+    backgroundList.innerHTML += createListItem(background.name);
+  });
+
+  //Populate Race List
+  races.forEach((race) => {
+    raceList.innerHTML += createListItem(race.name);
+  });
+
+  // Event Listeners
+  document.getElementById("classes").addEventListener("change", () => {
+    classBonus(document.getElementById("classes").value);
+  });
+  document.getElementById("races").addEventListener("change", () => {
+    raceBonus(document.getElementById("races").value);
+  });
+  document.getElementById("backgrounds").addEventListener("change", () => {
+    backgroundBonus(document.getElementById("backgrounds").value);
+  });
+  document.getElementById("update-btn").addEventListener("click", () => {
+    updateStats();
+  });
+  document.getElementById("ap-btn").addEventListener("click", () => {
+    openAPRoller();
+  });
+  document.getElementById("level-btn").addEventListener("click", () => {
+    levelUp();
+  });
+  skills.savingThrows.forEach((sThrow) => {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("class", "st-li");
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("class", "savingThrow");
+    input.setAttribute("id", sThrow.id);
+
+    input.addEventListener("click", () => {
+      addPro(sThrow.id, sThrow.stat);
+    });
+
+    const stOutput = document.createElement("p");
+    stOutput.setAttribute("class", "stMod");
+    stOutput.setAttribute("id", sThrow.id + "-mod");
+
+    listItem.appendChild(input);
+    listItem.appendChild(stOutput);
+    document.getElementById("saving-throws").appendChild(listItem);
+  });
+  skills.skills.forEach((skill) => {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("class", "skill-li");
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("class", "skill");
+    input.setAttribute("id", skill.id);
+
+    input.addEventListener("click", () => {
+      addPro(skill.id, skill.stat);
+    });
+
+    const stOutput = document.createElement("p");
+    stOutput.setAttribute("class", "skillMod");
+    stOutput.setAttribute("id", skill.id + "-mod");
+
+    listItem.appendChild(input);
+    listItem.appendChild(stOutput);
+    document.getElementById("skills").appendChild(listItem);
+  });
+
+  updateStats();
+}
+
+function createListItem(value) {
+  return `<option value=${value} />`;
+}
+
 function openAPRoller() {
   console.log("Opening AP roller...");
   const modal = document.getElementById("ap-modal");
@@ -381,5 +470,4 @@ function updateStats() {
     10 + character.stats.wis.mod + pro;
 }
 
-// characterialization of Page Values
-updateStats();
+initializePage();
